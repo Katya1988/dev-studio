c86b9d401896deb14ef23e8994e770d1663806c7
glebchekh <glebchekh@gmail.com>
2016-05-14 01:45:23 +0300

    Third commit Gleb

--- myjs.js
+++ myjs.js
@@ -1,4 +1,49 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('ul.tabs').tabs();
+
+    $("#loginform").validate({
+
+        rules:{
+
+            login:{
+                required: true,
+                maxlength: 15,
+            },
+            number:{
+                required: true,
+                number: true
+            },
+            email:{
+                required: true,
+                email: true,
+            },
+
+
+        },
+
+        messages: {
+
+            login:{
+                required: "Это поле обязательно для заполнения",
+                maxlength: "Максимальное число символов - 15",
+            },
+
+
+            number:{
+                required: "Это поле обязательно для заполнения",
+                number: "В номере могут быть только числа",
+            },
+            email:{
+                required: "Это поле обязательно для заполнения",
+                email: "Не верній email",
+            },
+
+        }
+
+    });
 });
+
+
+
+
