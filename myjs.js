8d6dcd7a696e0f5fe0659970aec4b4368ba78650
Katya1988 <quitesensual@gmail.com>
2016-05-30 20:01:22 +0300

    -tabs -button up - faded backgrounds

--- myjs.js
+++ myjs.js
@@ -1,6 +1,41 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('ul.tabs').tabs();
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if(height  > $(".blockBackground").offset().top) {
+            $("nav.mynav").css('background-color', 'rgba(153,204,204, 0.8)');
+        }
+
+        else {
+            $("nav.mynav").css('background-color', 'transparent');
+
+        }
+
+    });
+
+    $('body').append('<a href="#" id="go-top" title="Вверх">Вверх</a>');
+    $(function() {
+        $.fn.scrollToTop = function() {
+            $(this).hide().removeAttr("href");
+            if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+            var scrollDiv = $(this);
+            $(window).scroll(function() {
+                if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+                else $(scrollDiv).fadeIn("slow")
+            });
+            $(this).click(function() {
+                $("html, body").animate({scrollTop: 0}, "slow")
+            })
+        }
+    });
+
+    $(function() {
+        $("#go-top").scrollToTop();
+    });
+
+
+
 
     $("#loginform").validate({
 
